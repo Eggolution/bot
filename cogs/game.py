03d3026ef4636ea46126c108f7dafb3ab2bd18b0
Xenophloxic <64376702+Xenophloxic@users.noreply.github.com>
2020-11-24 20:02:00 -0800

    Update game.py

--- cogs/game.py
+++ cogs/game.py
@@ -26,12 +26,12 @@ class Game(commands.Cog):
     @commands.command()
     async def start(self, ctx):
         """Start your adventure"""
-        e = discord.Embed(timestamp=datetime.utcnow())
+        e = discord.Embed(timestamp=datetime.utcnow(), color=0x808080)
         e.set_author(name='Welcome to Eggolution!')
         e.add_field(name='What is Eggolution?',
                     value='Eggolution is a game where you level up to become the best. You can trade, battle, and choose your path.',
                     inline=False)
-        e.add_field(name='How do I start?', value='you can start using the e!play command.', inline=False)
+        e.add_field(name='How do I start?', value='you can start using the e!create command.', inline=False)
         e.add_field(name='How do I play?', value='Just start chatting! If you need more detailed help, use e!help',
                     inline=False)
         e.set_footer(text='Eggolution game')
@@ -39,54 +39,27 @@ class Game(commands.Cog):
         await ctx.send(embed=e)
 
     @commands.command()
-    async def play(self, ctx):
+    async def create(self, ctx):
         """Create your profile"""
-        mongo_url = "mongodb+srv://xeno:j5w1CrLgar6irElX@cluster0.1kvbu.mongodb.net/<dbname>?retryWrites=true&w=majority"
-        cluster = MongoClient(mongo_url)
-        db = cluster["Info"]
-        collection = db["userinfo"]
         author_id = ctx.message.author.id
         form = "egg"
         color = "none"
-        insert = {"_id": author_id, "form": form, "color": color, "XP": 0, "level": 0, "hp": 0, "atk": 0, "def": 0, "sp": 0}
-        await collection.insert_one(insert)
+        insert = {"_id": author_id, "credits": 0, "form": form, "color": color, "XP": 0, "level": 0, "hp": 0, "atk": 0, "def": 0, "sp": 0}
+        await self.bot.userinfo.insert(insert)
 
         await ctx.send('I have created your profile, use e!info to see yourself')
 
     @commands.command()
     async def info(self, ctx):
         """See your stats"""
-        mongo_url = "mongodb+srv://xeno:j5w1CrLgar6irElX@cluster0.1kvbu.mongodb.net/<dbname>?retryWrites=true&w=majority"
-        cluster = MongoClient(mongo_url)
-        db = cluster["Info"]
-        collection = db["userinfo"]
-        author_id = ctx.message.author.id
-        x = collection.find_one({"_id": author_id}, {"form": 1, "hp": 1, "atk": 1, "def": 1, "sp": 1, "color": 1})
+        x = await self.bot.userinfo.get_info(ctx.message.author.id)
+        print(x)
 
-        e = discord.Embed(timestamp=datetime.utcnow())
+        e = discord.Embed(timestamp=datetime.utcnow(), color=0x808080)
         e.set_thumbnail(url=ctx.message.author.avatar_url)
         if x["form"] == "egg":
             e.set_author(name=f'You are a {x["color"]} Egg!')
             e.set_image(url="https://media.discordapp.net/attachments/730191153206525955/766757785689260052/Untitled.png?width=517&height=560")
-        elif x["form"] == "egg":
-            e.set_author(name='Egg')
-            e.set_image(url="https://media.discordapp.net/attachments/730191153206525955/766757785689260052/Untitled.png?width=517&height=560")
-        elif x["form"] == "egg":
-            e.set_author(name='Egg')
-            e.set_image(
-                url="https://media.discordapp.net/attachments/730191153206525955/766757785689260052/Untitled.png?width=517&height=560")
-        elif x["form"] == "egg":
-            e.set_author(name='Egg')
-            e.set_image(
-                url="https://media.discordapp.net/attachments/730191153206525955/766757785689260052/Untitled.png?width=517&height=560")
-        elif x["form"] == "egg":
-            e.set_author(name='Egg')
-            e.set_image(
-                url="https://media.discordapp.net/attachments/730191153206525955/766757785689260052/Untitled.png?width=517&height=560")
-        elif x["form"] == "egg":
-            e.set_author(name='Egg')
-            e.set_image(
-                url="https://media.discordapp.net/attachments/730191153206525955/766757785689260052/Untitled.png?width=517&height=560")
         e.add_field(name=f'**Attack:** {x["atk"]}/100', value='‎', inline=False)
         e.add_field(name=f'**Defense:** {x["def"]}/100', value='‎', inline=False)
         e.add_field(name=f'**Health:** {x["hp"]}/100', value='‎', inline=False)
