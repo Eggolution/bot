b936f08d9729954a98931f8417be4d68b1a19d67
Xenophloxic <64376702+Xenophloxic@users.noreply.github.com>
2020-11-24 19:58:43 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,49 +1,54 @@
-import asyncio
-import os
-import traceback
-from pathlib import Path
-from random import randint
-
 import motor.motor_asyncio
-from pymongo import MongoClient
 
 from cogs.mongo import Document
 
 import DiscordUtils
-
-from secrets import TOKEN
+from datetime import datetime
 import discord
 from discord.ext import commands
-from discord import Member, channel, message
-from discord.ext.commands import has_permissions, MissingPermissions, Bot, has_guild_permissions
-import json
-from pretty_help import PrettyHelp
 
 intents = discord.Intents.default()
 intents.members = True
 
 
-def get_prefix(bot, message):
-    with open('cogs/prefixes/prefixes.json', 'r') as f:
-        data = json.load(f)
-    if not str(message.channel.id) in data:
-        return commands.when_mentioned_or('e!', 'E!', '~')(bot, message)
-    return commands.when_mentioned_or(data[str(message.guild.id)])(bot, message)
+async def get_prefix(bot, message):
+    if message.author.id == 393480172638044160:
+        return commands.when_mentioned_or('t')(bot, message)
+
+    else:
+        if not message.guild:
+            return commands.when_mentioned_or('t')(bot, message)
+
+        try:
+            data = await bot.config.find(message.guild.id)
+
+            if not data or "prefix" not in data:
+                return commands.when_mentioned_or('t')(bot, message)
+            return commands.when_mentioned_or(data["prefix"].lower(), data["prefix"].upper())(bot, message)
+        except:
+            return commands.when_mentioned_or('t')(bot, message)
 
 
-bot: Bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True, owners_id=393480172638044160,
+bot = commands.Bot(command_prefix=get_prefix, case_insensitive=True, owners_id=393480172638044160,
                          intents=intents)
+bot.snipes = {}
 
-bot.help_command = PrettyHelp(color=0x808080, active=100)
+bot.launch_time = datetime.utcnow()
+
 
 tracker = DiscordUtils.InviteTracker(bot)
 
-ext = ['cogs.config', 'cogs.game', 'cogs.fun', 'cogs.other', 'cogs.mod', 'jishaku', 'cogs.invite']
+ext = ['cogs.config', 'cogs.game', 'cogs.fun', 'cogs.other', 'cogs.mod', 'jishaku', 'cogs.invite', 'cogs.events', 'cogs.music', 'cogs.help']
 if __name__ == '__main__':
     bot.connection_url = "mongodb+srv://xeno:j5w1CrLgar6irElX@cluster0.1kvbu.mongodb.net/<dbname>?retryWrites=true&w=majority"
     bot.mongo = motor.motor_asyncio.AsyncIOMotorClient(str(bot.connection_url))
     bot.db = bot.mongo["Info"]
     bot.invites = Document(bot.db, "invites")
+    bot.invitee = Document(bot.db, "invitee")
+    bot.warns = Document(bot.db, "warns")
+    bot.userinfo = Document(bot.db, "userinfo")
+    bot.config = Document(bot.db, "prefixes")
+
     for extension in ext:
         bot.load_extension(extension)
 
